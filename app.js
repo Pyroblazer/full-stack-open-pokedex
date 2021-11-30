749b79bbd36dd84231e0ef2b06bf135a598f9da0
Pyroblazer <ignatiustimothymanullang@gmail.com>
2021-11-30 21:21:53 +0700

    finish 11.11-11.12

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const process = require('process')
+const express = require('express');
+const process = require('process');
 
-const app = express()
-
-app.use(express.static('dist'))
-
-app.get('/health', (_, res) => {
-  res.send('ok')
-})
-
-app.get('/version', (_, res) => {
-  res.send('1') // change this string to ensure a new version deployed
-})
+const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
+
+app.get('/health', (_, res) => {
+  res.send('ok');
+});
+
+app.get('/version', (_, res) => {
+    res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
-})
+});
+
+
+  
